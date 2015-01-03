ed3b4b38fcde560bf3f4763a5480383d034c6bdb
laliaga <laliaga>
2015-01-03 06:33:36 +0000

    Improvements in this code to have the option to run in both: dk2nu and nudata input formats.  Also, checking of the inputs and a help usage menu

--- src/doRW.C
+++ src/doRW.C
@@ -2,6 +2,7 @@
 #include "ReweightDriver.h"
 #include "InteractionData.h"
 #include "TargetData.h"
+#include "ParticlesThroughVolumesData.h"
 #include "InteractionChainData.h"
 #include "ParameterTable.h"
 #include "CentralValuesAndUncertainties.h"
@@ -19,9 +20,9 @@
 #include <vector>
 #include <iostream>
 #include <stdlib.h>
+#include <iomanip>
 
 //Some constants::
-
 
 const int Nuniverses = 100;
 
@@ -44,31 +45,40 @@ class nu_g4numi;
 class Numi2Pdg;
 
 int idx_hel(int pdgdcode);
+bool check_inputs(const char* input_format,const char* par_option,const char* beammode,const char* runnumber, const char* cnent);
+void print_usage();
 
 /*!
  * Run the reweighting for a single file cnent number of events, for a 
  * particular MIPP covariance matrix. 
  *
+ * input_format: select between dk2nu and nudata ntuple formats
  * par_option: select the covariance matrix by passing in the name of the xml file.
  * beammode: select the beam configuration, i.e., LE010z185i, this is only used to select the input file.
  * runnumber: select the run number, only used to select the input file
  * cnent: number of events we want to process from the file.
  */
-void doRW(const char* par_option,const char* beammode,const char* runnumber, const char* cnent){ 
+void doRW(const char* input_format,const char* par_option,const char* beammode,const char* runnumber, const char* cnent){ 
+  
+  if(!check_inputs(input_format,par_option,beammode,runnumber,cnent)){
+    exit(1);
+  }
 
   const char* ntupleDir = "/minerva/data/flux_hadron_samples/flux/g4numi/v4_test"; 
+  //this is temporary because these are the ntuples with the new variables: 
+  const char* tmp_dk2nuDir = "/minerva/data/users/laliaga/flux/TESTVOL/MOD"; 
   const char* thisDir = getenv("PPFX_DIR");
   //  const char* OutputDir = "/minerva/data/users/laliaga/FLUX_PACK/GRID";
   const char* OutputDir=thisDir;
   
   int nent = atoi(cnent);
-
   int irun = atoi(runnumber);
+  bool is_dk2nu = strcmp(input_format,"dk2nu")==0;
   //  TFile* fOut = new TFile(Form("%s/%s/%s/fOut_%s_%s_%04d.root",OutputDir,beammode,par_option,par_option,beammode,irun),"recreate");
 
   std::cout<<"Making an output file to store histograms"<<std::endl;
 
-  TFile* fOut = new TFile(Form("test%d_%dentries_with1000univ.root",irun,nent),"recreate");
+  TFile* fOut = new TFile(Form("test%d_%dentries_with1000univ_%s.root",irun,nent,input_format),"recreate");
   std::cout<<"File name: "<<fOut->GetName()<<std::endl;
 
   HistoContainer* histos =  HistoContainer::getInstance();
@@ -98,14 +108,39 @@ void doRW(const char* par_option,const char* beammode,const char* runnumber, con
   }
   
   //Loading ntuples:
-  TChain* chain_in = new TChain("nudata");
-  std::cout<<" Adding ntuple at: "<< Form("%s/%s/g4numiv4_%s_%04d_0001.root",ntupleDir,beammode,beammode,irun)<<std::endl;
-  chain_in->Add(Form("%s/%s/g4numiv4_%s_%04d_0001.root",ntupleDir,beammode,beammode,irun));
-  
-  nu_g4numi* nu = new nu_g4numi(chain_in);
-  int nentries  = nu->nentries;
-  int ntrees    = nu->ntrees;
+  TChain* chain_evts;
+  TChain* chain_meta;
+  nu_g4numi* nu;
+  bsim::Dk2Nu*  dk2nu;
+  bsim::DkMeta*  dkmeta;
+  int nentries  = 0;
+  int ntrees    = 0;
+
+  if(!is_dk2nu){
+    chain_evts = new TChain("nudata");
+    chain_evts->Add(Form("%s/%s/g4numiv4_%s_%04d_0001.root",ntupleDir,beammode,beammode,irun));
+    nu = new nu_g4numi(chain_evts);
+    std::cout<<" Adding ntuple at: "<< Form("%s/%s/g4numiv4_%s_%04d_0001.root",ntupleDir,beammode,beammode,irun)<<std::endl;
+    nentries  = nu->nentries;
+    ntrees    = nu->ntrees;
+  }
+  else{
+    chain_evts = new TChain("dk2nuTree");  
+    std::cout<<" Adding ntuple at: "<<Form("%s/g4numiv5_minerva1_%s_%d_0005.root",tmp_dk2nuDir,beammode,irun) <<std::endl;
+    chain_evts->Add(Form("%s/g4numiv5_minerva1_%s_%d_0005.root",tmp_dk2nuDir,beammode,irun));
+    dk2nu  = new bsim::Dk2Nu;   
+    chain_evts->SetBranchAddress("dk2nu",&dk2nu);
+    nentries  = chain_evts->GetEntries();
+    ntrees    = chain_evts->GetNtrees();
+
+    chain_meta = new TChain("dkmetaTree");  
+    chain_meta->Add(Form("%s/g4numiv5_minerva1_%s_%d_0005.root",tmp_dk2nuDir,beammode,irun));
+    dkmeta  = new bsim::DkMeta;
+    chain_meta->SetBranchAddress("dkmeta",&dkmeta);    
+  }
+
   std::cout<<"Ntrees: "<<ntrees<<", entries: "<<nentries<<std::endl;
+
   
   //Central Values And Uncertainties:
   CentralValuesAndUncertainties* cvu = CentralValuesAndUncertainties::getInstance();;
@@ -132,33 +167,52 @@ void doRW(const char* par_option,const char* beammode,const char* runnumber, con
     vec_rws.push_back(new ReweightDriver(ii,cvPars,univPars));    
   }
   std::cout<<"Done configuring universes"<<std::endl;
-
+  
   std::vector<double> wgts;
   for(int ii=0;ii<Nuniverses;ii++){
     wgts.push_back(1.0);
   }
 
-
   std::cout<<"Running over "<<nent<<" entries"<<std::endl;
   
   // for(int ii=0;ii<red_nentries;ii++){  
   for(int ii=0;ii<nent;ii++){  
-    //  if(ii%1000==0)std::cout<<ii/1000<<" k evts"<<std::endl;
-    std::cout<<"On entry "<<ii<<std::endl;
-    nu->GetEntry(ii);     
-
+     if(ii%1000==0)std::cout<<ii/1000<<" k evts"<<std::endl;
+    //  std::cout<<"On entry "<<ii<<std::endl;
+    if(!is_dk2nu)nu->GetEntry(ii);     
+    else{
+      chain_evts->GetEntry(ii);     
+      chain_meta->GetEntry(ii);     
+    }
     // create an interaction chain from the data record
-    InteractionChainData inter_chain(nu,"le010z","185i");
-    inter_chain.print(std::cout);
-
+    InteractionChainData* inter_chain;
+    if(!is_dk2nu)inter_chain = new InteractionChainData(nu,"le010z","185i");
+    else inter_chain = new InteractionChainData(dk2nu,dkmeta);
+    
+    //inter_chain->print(std::cout);
+        
     // histogram the flux, without any reweighting
-    double fluxWGT = (nu->Nimpwt)*(nu->NWtNear[0]);    
-    int nuidx = idx_hel(numi2pdg->GetPdg(nu->Ntype));
-    if(nuidx>=0)hnom[nuidx]->Fill(nu->NenergyN[0],fluxWGT);
+    double fluxWGT = 1.0;
+    if(!is_dk2nu)fluxWGT = (nu->Nimpwt)*(nu->NWtNear[0])/3.1416;    
+    else fluxWGT = ( (dk2nu->nuray)[2].wgt )*(dk2nu->decay.nimpwt)/3.1416;
+    
+    int nuidx = -1;
+    double nuenergy = -1.0;
+    if(!is_dk2nu){
+      nuidx = idx_hel(numi2pdg->GetPdg(nu->Ntype));
+      nuenergy = nu->NenergyN[0];
+    }
+    else{
+      nuidx = idx_hel(dk2nu->decay.ntype);
+      nuenergy = (dk2nu->nuray)[2].E; 
+    }
+    
+    if(nuidx>=0)hnom[nuidx]->Fill(nuenergy,fluxWGT);
+    
     
     bool is_mipp = false;        
     for(int jj=0;jj<Nuniverses;jj++){
-      wgts[jj] = vec_rws[jj]->calculateWeight(inter_chain);
+      wgts[jj] = vec_rws[jj]->calculateWeight(*inter_chain);
       if(fabs(wgts[jj]-1.0)>1.e-15 && jj==0){is_mipp=true;}
       
       std::map<std::string,double> map_info = info->GetInfo();
@@ -166,14 +220,14 @@ void doRW(const char* par_option,const char* beammode,const char* runnumber, con
 
       double univWGT = fluxWGT*wgts[jj];
       if(nuidx>=0){
-	vh_rw[nuidx][jj]->Fill(nu->NenergyN[0],univWGT);
+	vh_rw[nuidx][jj]->Fill(nuenergy,univWGT);
 	
 	if(is_mipp){
-	  vh_mipp_rw[nuidx][jj]->Fill(nu->NenergyN[0],univWGT);
+	  vh_mipp_rw[nuidx][jj]->Fill(nuenergy,univWGT);
 	  
 	  for(it_info=map_info.begin();it_info!=map_info.end();it_info++){
 	    double checkwgt = fluxWGT*(it_info->second);
-	    histos->plot1D(nu->NenergyN[0],it_info->first,"",NbinsE,emin,emax,checkwgt);
+	    histos->plot1D(nuenergy,it_info->first,"",NbinsE,emin,emax,checkwgt);
 	  }
 	  
 	}
@@ -184,8 +238,7 @@ void doRW(const char* par_option,const char* beammode,const char* runnumber, con
       
     }
     
-    if(is_mipp && nuidx>=0)hnom_mipp[nuidx]->Fill(nu->NenergyN[0],fluxWGT);
-    
+    if(is_mipp && nuidx>=0)hnom_mipp[nuidx]->Fill(nuenergy,fluxWGT);
     
   }
   
@@ -258,6 +311,7 @@ void doRW(const char* par_option,const char* beammode,const char* runnumber, con
        */
        
      }
+
    }
    
    
@@ -274,12 +328,58 @@ int idx_hel(int pdgcode){
   return idx;
 }
 
+void print_usage(){
+  std::cout<<"bin/doRW [input_format] [par_option] [beammode] [runnumber] [cnent]" <<std::endl;
+  std::cout<<"===================================================================" <<std::endl;
+  std::cout<<"input_format: dk2nu or nudata"<<std::endl;
+  std::cout<<"par_option  : noCorr, low, ... look at the uncertainty directory"<<std::endl;
+  std::cout<<"beammode    : le010z185i or le010z-185i"<<std::endl;
+  std::cout<<"runnumber   : [0-499]"<<std::endl;
+  std::cout<<"cnent       : [>=0]"<<std::endl;  
+}
+
+bool check_inputs(const char* input_format,const char* par_option,const char* beammode,const char* runnumber, const char* cnent){
+
+  //input_format:
+  bool good_format = false;
+  if(strcmp(input_format,"dk2nu")==0 || strcmp(input_format,"nudata")==0)good_format = true;
+
+  //par_option:
+  bool good_par = false;
+  if(strcmp(par_option,"noCorr")==0 || strcmp(par_option,"low")==0 )        good_par = true;
+  if(strcmp(par_option,"medium")==0 || strcmp(par_option,"mediumBYDET")==0) good_par = true;
+  if(strcmp(par_option,"high")==0   || strcmp(par_option,"highBYDET")==0)   good_par = true;
+  if(strcmp(par_option,"uhigh")==0  || strcmp(par_option,"uhighBYDET")==0)  good_par = true;
+  
+  bool good_bmode = false;
+  if(strcmp(beammode,"le010z185i")==0 || strcmp(beammode,"le010z-185i")==0)good_bmode = true;
+  
+  bool good_runN = false;
+  if(atoi(runnumber)>=0 && atoi(runnumber)<500)good_runN = true;
+  
+  bool good_nent = false;
+  if(atoi(cnent)>=0)good_nent = true;
+  
+  bool good_args = good_format && good_par && good_bmode && good_runN && good_nent;
+  if(!good_args){
+    std::cout<<"No good arguments"<<std::endl;
+    print_usage();
+  }
+  return good_args;
+  
+  
+}
+
 ////////////////////////////////
 #ifndef __CINT__
 int main(int argc, const char* argv[]){
   
-
-  doRW(argv[1],argv[2],argv[3],argv[4]);
+  if(argc!=6){
+    std::cout<<"We need "<<argc-1<<" arguments to execute this script. Check the help menu:"<<std::endl;
+    print_usage();
+    exit (1);
+  }
+  doRW(argv[1],argv[2],argv[3],argv[4],argv[5]);
   return 0;
 }
 #endif
