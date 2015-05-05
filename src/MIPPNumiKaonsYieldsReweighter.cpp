737d09a8830a6ff9640874d9f044354db679ef0b
laliaga <laliaga>
2015-05-05 01:19:25 +0000

    First version of the MIPP NuMI Kaon Reweighter. For this version, I am not treating Sharaon and J. Paley's results are not correlated.

--- src/MIPPNumiKaonsYieldsReweighter.cpp
+++ src/MIPPNumiKaonsYieldsReweighter.cpp
@@ -86,7 +86,94 @@ namespace NeutrinoFluxReweight{
     return this_nodes;
   }
   double MIPPNumiKaonsYieldsReweighter::calculateWeight(const InteractionChainData& aa){
+    
+    MIPPNumiYieldsBins*  MIPPbins =  MIPPNumiYieldsBins::getInstance();
+    DataHistos*          dtH      =  DataHistos::getInstance();
+    double low_value = 1.e-18;      
+
+    TargetData tar = aa.tar_info;
+    if(tar.Tar_pdg != 321 && tar.Tar_pdg != -321)return 1.0;
+    int binID = MIPPbins->BinID(tar.Pz,tar.Pt,tar.Tar_pdg);
+    if(binID<0){
+      std::cout<<"BINID ZERO:" <<tar.Pz<<" "<<tar.Pt<<" " <<tar.Tar_pdg<<std::endl;
       return 1.0;
+    }
+    //Now looking for pion bin ID:
+    int pi_wanted = 211;
+    if(tar.Tar_pdg == -321)pi_wanted = -211;
+    int pi_binID = MIPPbins->BinID(tar.Pz,tar.Pt,pi_wanted);
+    if(pi_binID<0){
+      std::cout<<"There is not MIPP pion matching the kaon kinematics "<<pi_wanted<<" "<<tar.Pz<<" "<<tar.Pt<<std::endl;
+      return 1.0;
+    }
+    
+    const char* pitype = "pip";
+    const char* kptype   = "kap_pip";
+    if(tar.Tar_pdg == -321){
+      pitype = "pim"; 
+      kptype = "kam_pim";
+    }
+    char namepar_pi_sys[100]; char namepar_pi_sta[100];
+    char namepar_kp_sys[100]; char namepar_kp_sta[100];
+    sprintf(namepar_pi_sys,"MIPP_NuMI_%s_sys_%d",pitype,pi_binID);
+    sprintf(namepar_kp_sys,"MIPP_NuMI_%s_sys_%d",kptype,binID);
+    sprintf(namepar_pi_sta,"MIPP_NuMI_%s_stats_%d",pitype,pi_binID);
+    sprintf(namepar_kp_sta,"MIPP_NuMI_%s_stats_%d",kptype,binID);
+    
+    ///CV:
+    std::map<std::string, double> cv_table = cvPars.table;
+    std::map<std::string, double>::iterator cv_it = cv_table.begin();
+    
+    cv_it = cv_table.begin();    
+    cv_it = cv_table.find(std::string(namepar_pi_sys));
+    double data_cv_pi = cv_it->second;
+    
+    cv_it = cv_table.begin();
+    cv_it = cv_table.find(std::string(namepar_kp_sys));
+    double data_cv_kp = cv_it->second;
+    
+    //UNIV:
+    std::map<std::string, double> this_table = univPars.table;
+    std::map<std::string, double>::iterator it = this_table.begin();
+
+    it = this_table.begin();
+    it = this_table.find(std::string(namepar_pi_sys));
+    double data_univ_pi_sys = it->second;
+    
+    it = this_table.begin();
+    it = this_table.find(std::string(namepar_kp_sys));
+    double data_univ_kp_sys = it->second;
+
+    it = this_table.begin();
+    it = this_table.find(std::string(namepar_pi_sta));
+    double data_univ_pi_sta = it->second;
+
+    it = this_table.begin();
+    it = this_table.find(std::string(namepar_kp_sta));
+    double data_univ_kp_sta = it->second;
+    
+
+    //Now, looking for the MC value:
+    int i_use = 2;
+    if(tar.Tar_pdg == -321)i_use = 3;
+    double binC = dtH->hMIPP_MC[i_use]->GetBinContent(dtH->hMIPP_MC[i_use]->FindBin(tar.Pz,tar.Pt) );
+    if(binC<low_value){
+      std::cout<<"LOW MC VAL: "<<binC <<std::endl;
+      return 1.0;
+    }
+
+    //First assumption: no correlation at all between Sharon & Jon results:
+    double K_data_cv = data_cv_pi*data_cv_kp;
+    double K_data_univ_sys = data_univ_pi_sys*data_univ_kp_sys;
+    double K_data_univ_sta = data_univ_pi_sta*data_univ_kp_sta;
+
+    if(K_data_cv<low_value || K_data_univ_sys<low_value || K_data_univ_sta<low_value){
+      std::cout<<"LOW DATA VAL: "<<K_data_cv<<" "<<K_data_univ_sys<<" "<<K_data_univ_sta<<" "<<tar.Tar_pdg<<" "<<tar.Pz<<" "<<tar.Pt<<std::endl;
+      return 1.0;
+    }
+    
+    return (K_data_univ_sys + K_data_univ_sta - K_data_cv)/binC;
+
   }
 
 }
